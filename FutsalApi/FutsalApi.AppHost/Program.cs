ded808e139cca956f59582280c3d0d2573c5beea
0ankit0 <ankitpaudel.1373@gmail.com>
2025-03-25 07:31:28 +0545

    Add configuration files and initial setup for Futsal API service

--- FutsalApi/FutsalApi.AppHost/Program.cs
+++ FutsalApi/FutsalApi.AppHost/Program.cs
@@ -1,3 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.FutsalApi_ApiService>("apiservice");
+
+
 builder.Build().Run();
