8d1fab47004df78e5f1fc82939e6b580f9298c0b
0ankit0 <ankitpaudel.1373@gmail.com>
2025-03-27 14:08:23 +0545

    Add PostgreSQL support and FluentValidation integration to Futsal API service

--- FutsalApi/FutsalApi.ApiService/Program.cs
+++ FutsalApi/FutsalApi.ApiService/Program.cs
@@ -1,10 +1,22 @@
+using System.Reflection;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using FutsalApi.ApiService.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
+
+// Get connection string from appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Add DbContext with PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 
 builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
 builder.Services.AddAuthorizationBuilder();
@@ -15,6 +27,12 @@ builder.Services.AddIdentity<Users, Roles>()
 
 // Add services to the container.
 builder.Services.AddProblemDetails();
+
+builder.Services.AddFluentValidationAutoValidation() // Enables automatic validation
+                .AddFluentValidationClientsideAdapters(); // Enables client-side validation for MVC
+
+builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly()); // Scans & registers all validators
+
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
