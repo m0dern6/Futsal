71c3961f09fad1f2e4d8c92530edfd35b7b6accc
0ankit0 <ankitpaudel.1373@gmail.com>
2025-04-07 05:07:20 +0545

    Refactor IGenericRepository interface to use predicates for GetById, Update, and Delete methods

--- FutsalApi/FutsalApi.ApiService/Repositories/Interfaces/IGenericRepository.cs
+++ FutsalApi/FutsalApi.ApiService/Repositories/Interfaces/IGenericRepository.cs
@@ -5,9 +5,9 @@ namespace FutsalApi.ApiService.Repositories;
 public interface IGenericrepository<T> where T : class
 {
     Task<IEnumerable<T>> GetAllAsync(int page = 1, int pageSize = 10);
-    Task<T?> GetByIdAsync(int id);
+    Task<T?> GetByIdAsync(Expression<Func<T, bool>> predicate);
     Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
     Task<T> CreateAsync(T entity);
-    Task<T> UpdateAsync(int id, T entity);
-    Task<bool> DeleteAsync(int id);
+    Task<T> UpdateAsync(Expression<Func<T, bool>> predicate, T entity);
+    Task<bool> DeleteAsync(Expression<Func<T, bool>> predicate);
 }
