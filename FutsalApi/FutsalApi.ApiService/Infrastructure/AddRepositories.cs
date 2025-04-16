dd4447fd5438e0494d6a647a2fc0999bbc3e45b9
0ankit0 <ankitpaudel.1373@gmail.com>
2025-04-16 05:51:30 +0545

    Add repository extension for automatic registration of GenericRepository implementations

---
+++ FutsalApi/FutsalApi.ApiService/Infrastructure/AddRepositories.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Reflection;
+
+using FutsalApi.ApiService.Repositories;
+
+namespace FutsalApi.ApiService.Infrastructure;
+
+public static class RepositoryExtension
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services, Assembly assembly)
+    {
+        // Register all repositories in the assembly that inherits from GenericRepository
+        var repositoryTypes = assembly.GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && t.BaseType != null && t.BaseType.IsGenericType && t.BaseType.GetGenericTypeDefinition() == typeof(GenericRepository<>));
+
+        // Register each repository type with its corresponding interface
+        foreach (var repositoryType in repositoryTypes)
+        {
+            var interfaceType = repositoryType.GetInterfaces().FirstOrDefault(i => !i.IsGenericType);
+            if (interfaceType != null)
+            {
+                // Console.WriteLine($"Registering {interfaceType.Name} with {repositoryType.Name}");
+                services.AddScoped(interfaceType, repositoryType);
+            }
+        }
+        return services;
+    }
+
+}
