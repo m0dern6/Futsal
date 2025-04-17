90db32ad921c698d18d6e2521b14755dc773ae24
0ankit0 <ankitpaudel.1373@gmail.com>
2025-04-17 06:36:47 +0545

    Refactor FutsalGroundRepository and IFutsalGroundRepository to return FutsalGroundResponse objects for improved data handling

--- FutsalApi/FutsalApi.ApiService/Repositories/ConcreteClasses/FutsalGroundRepository.cs
+++ FutsalApi/FutsalApi.ApiService/Repositories/ConcreteClasses/FutsalGroundRepository.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq.Expressions;
 
 using FutsalApi.ApiService.Data;
+using FutsalApi.ApiService.Models;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +15,7 @@ public class FutsalGroundRepository : GenericRepository<FutsalGround>, IFutsalGr
     {
         _dbContext = dbContext;
     }
-    public override async Task<IEnumerable<FutsalGround>> GetAllAsync(int page = 1, int pageSize = 10)
+    public async new Task<IEnumerable<FutsalGroundResponse>> GetAllAsync(int page = 1, int pageSize = 10)
     {
         if (page <= 0 || pageSize <= 0)
         {
@@ -24,7 +26,37 @@ public class FutsalGroundRepository : GenericRepository<FutsalGround>, IFutsalGr
             .OrderByDescending(g => g.CreatedAt)
             .Skip((page - 1) * pageSize)
             .Take(pageSize)
+            .Select(g => new FutsalGroundResponse
+            {
+                Id = g.Id,
+                Name = g.Name,
+                Location = g.Location,
+                OwnerId = g.OwnerId,
+                PricePerHour = g.PricePerHour,
+                OpenTime = g.OpenTime,
+                CloseTime = g.CloseTime,
+                CreatedAt = g.CreatedAt,
+                OwnerName = g.Owner.UserName!
+            })
             .ToListAsync();
+    }
+    public async new Task<FutsalGroundResponse?> GetByIdAsync(Expression<Func<FutsalGround, bool>> predicate)
+    {
+        return await _dbContext.FutsalGrounds
+            .Where(predicate)
+            .Select(g => new FutsalGroundResponse
+            {
+                Id = g.Id,
+                Name = g.Name,
+                Location = g.Location,
+                OwnerId = g.OwnerId,
+                PricePerHour = g.PricePerHour,
+                OpenTime = g.OpenTime,
+                CloseTime = g.CloseTime,
+                CreatedAt = g.CreatedAt,
+                OwnerName = g.Owner.UserName!
+            })
+            .FirstOrDefaultAsync();
     }
 
 }
--- FutsalApi/FutsalApi.ApiService/Repositories/Interfaces/IFutsalGroundRepository.cs
+++ FutsalApi/FutsalApi.ApiService/Repositories/Interfaces/IFutsalGroundRepository.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Linq.Expressions;
 
 using FutsalApi.ApiService.Data;
+using FutsalApi.ApiService.Models;
 
 namespace FutsalApi.ApiService.Repositories;
 
 public interface IFutsalGroundRepository : IGenericRepository<FutsalGround>
 {
+    new Task<IEnumerable<FutsalGroundResponse>> GetAllAsync(int page = 1, int pageSize = 10);
+    new Task<FutsalGroundResponse?> GetByIdAsync(Expression<Func<FutsalGround, bool>> predicate);
 
 }
