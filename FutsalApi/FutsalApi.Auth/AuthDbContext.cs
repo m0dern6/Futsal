3d722ced0358cba3975ffc8a121933ce88dbc520
0ankit0 <ankitpaudel.1373@gmail.com>
2025-05-02 06:45:21 +0545

    Add AuthDbContext class with GeneralSettings DbSet for configuration management

---
+++ FutsalApi/FutsalApi.Auth/AuthDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FutsalApi.Auth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FutsalApi.Auth;
+
+public class AuthDbContext : IdentityDbContext<User, Role, string, IdentityUserClaim<string>, UserRole, IdentityUserLogin<string>, IdentityRoleClaim<string>, IdentityUserToken<string>>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<GeneralSetting> GeneralSettings { get; set; }
+}
