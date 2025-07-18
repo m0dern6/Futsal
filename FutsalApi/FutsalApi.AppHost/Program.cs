cb813d6cce84f4b8ff23e9d855ffd7259a4e2dd1
0ankit0 <ankitpaudel.1373@gmail.com>
2025-07-18 15:28:23 +0545

    Upgrade dependencies and switch to PostgreSQL
    Updated Aspire.Hosting packages to version 9.3.1 and added PostgreSQL support. Replaced Redis cache setup with a PostgreSQL database named "futsaldb" in `Program.cs`.
    
    Modified `docker-compose.yaml` to include services for Redis and PostgreSQL with appropriate configurations. Updated `aspirate-state.json` and `aspirate.json` to reflect changes in output format and repository prefix.
    
    Revised `manifest.json` to enhance connection strings and add new parameters for cache and database passwords. Overall, these changes improve dependency management and service configuration.

--- FutsalApi/FutsalApi.AppHost/Program.cs
+++ FutsalApi/FutsalApi.AppHost/Program.cs
@@ -1,8 +1,17 @@
-var builder = DistributedApplication.CreateBuilder(args);
+﻿var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.FutsalApi_ApiService>("apiservice");
+var db = builder.AddPostgres("db").WithPgAdmin();
+
+var futsaldb = db.AddDatabase("futsaldb");
+
+var apiService = builder.AddProject<Projects.FutsalApi_ApiService>("apiservice")
+    .WithHttpEndpoint(port:5000,name:"Http5000")
+    .WithHttpsEndpoint(port:5001,name:"Https5001")
+    .WithReference(db)
+    .WithReference(cache)
+    .WaitFor(cache);
 
 
 builder.Build().Run();
