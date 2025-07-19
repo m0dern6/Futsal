71c43f2ee7aaa1ba85a870bfc4fe5d178bc68259
0ankit0 <ankitpaudel.1373@gmail.com>
2025-07-19 14:16:57 +0545

    Remove HTTPS endpoint configuration from API service and update Docker Compose to eliminate unused HTTPS ports

--- FutsalApi/FutsalApi.AppHost/Program.cs
+++ FutsalApi/FutsalApi.AppHost/Program.cs
@@ -8,7 +8,7 @@ var futsaldb = db.AddDatabase("futsaldb");
 
 var apiService = builder.AddProject<Projects.FutsalApi_ApiService>("apiservice")
     .WithHttpEndpoint(port: 8000, name: "Http8000")
-    .WithHttpsEndpoint(port: 8001, name: "Https8001")
+    // .WithHttpsEndpoint(port: 8001, name: "Https8001")
     .WithReference(futsaldb)
     .WaitFor(futsaldb)
     .WithReference(cache)
