b79712d61b71ed097374d4cfa0e4a4da0b38fdda
0ankit0 <ankitpaudel.1373@gmail.com>
2025-07-25 09:13:05 +0545

    Update project configuration and enhance UI with Bootstrap integration

--- FutsalApi/FutsalApi.UI/FutsalApi.UI/MauiProgram.cs
+++ FutsalApi/FutsalApi.UI/FutsalApi.UI/MauiProgram.cs
@@ -21,6 +21,8 @@ public static class MauiProgram
 
         builder.Services.AddMauiBlazorWebView();
 
+        builder.Services.AddBlazorBootstrap();
+
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
         builder.Logging.AddDebug();
