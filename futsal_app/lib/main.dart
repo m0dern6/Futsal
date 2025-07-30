c3b0baa08fd2e063d3c1a80f2b668a8fdba560e9
m0dern6 <na.ab.en.153@gmail.com>
2025-07-30 22:11:59 +0545

    native splash and internet enabled

--- futsal_app/lib/main.dart
+++ futsal_app/lib/main.dart
@@ -3,9 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:futsalpay/core/config/bloc_provider.dart';
 import 'package:futsalpay/core/config/repository_provider.dart';
 import 'package:futsalpay/core/router/routes.dart';
-import 'package:futsalpay/core/services/api_service.dart';
-import 'package:futsalpay/features/auth/data/repository/auth_repository_impl.dart';
-import 'package:futsalpay/features/auth/domain/repository/auth_repository.dart';
 
 void main() {
   runApp(const MyApp());
