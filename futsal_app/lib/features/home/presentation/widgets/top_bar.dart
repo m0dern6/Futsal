4d9a1069b1221738a87cf1b7f7522653ec487b44
m0dern6 <na.ab.en.153@gmail.com>
2025-08-09 12:52:32 +0545

    displayed username

--- futsal_app/lib/features/home/presentation/widgets/top_bar.dart
+++ futsal_app/lib/features/home/presentation/widgets/top_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:futsalpay/core/config/dimension.dart';
+import 'package:futsalpay/shared/user_info/bloc/user_info_bloc.dart';
 
 class TopBar extends StatelessWidget {
   const TopBar({super.key});
@@ -7,35 +9,41 @@ class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Dimension.init(context);
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          'Hello, Modern',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: Dimension.font(20),
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        Column(
+    return BlocBuilder<UserInfoBloc, UserInfoState>(
+      builder: (context, state) {
+        final user = state is UserInfoLoaded ? state.userInfo : null;
+        final username = user?.username ?? 'Guest';
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Image.asset(
-              'assets/logo/inapplogo.png',
-              width: Dimension.width(65),
-              height: Dimension.height(35),
-            ),
             Text(
-              'Futsal Pay',
+              'Hello, $username',
               style: TextStyle(
-                color: Color(0xff618E6A),
-                fontSize: Dimension.font(14),
+                color: Colors.white,
+                fontSize: Dimension.font(20),
                 fontWeight: FontWeight.bold,
               ),
             ),
+            Column(
+              children: [
+                Image.asset(
+                  'assets/logo/inapplogo.png',
+                  width: Dimension.width(65),
+                  height: Dimension.height(35),
+                ),
+                Text(
+                  'Futsal Pay',
+                  style: TextStyle(
+                    color: Color(0xff618E6A),
+                    fontSize: Dimension.font(14),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
           ],
-        ),
-      ],
+        );
+      },
     );
   }
 }
