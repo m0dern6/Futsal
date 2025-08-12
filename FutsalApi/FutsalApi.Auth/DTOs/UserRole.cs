60ff6a089c15e421507895c0ce9c63a08aca336b
0ankit0 <ankitpaudel.1373@gmail.com>
2025-08-12 06:01:31 +0545

    Refactor authentication and authorization components
    Removed various classes, methods, and properties related to authentication and authorization in the `FutsalApi.Auth` project. This includes the deletion of `AuthDbContext`, user and role models, and user management request/response classes. Significant portions of the authentication logic in `Auth.cs` have also been commented out or removed, indicating a potential redesign. Additionally, several package references have been removed from the project file, impacting application functionality.

--- FutsalApi/FutsalApi.Auth/DTOs/UserRole.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace FutsalApi.Auth.Models;
-
-public class UserRole : IdentityUserRole<string>
-{
-
-}
--- FutsalApi/FutsalApi.Auth/Models/RefreshRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace FutsalApi.Auth.Models;
-
-public class RefreshRequest
-{
-    [Required]
-    public string RefreshToken { get; set; } = string.Empty;
-}
