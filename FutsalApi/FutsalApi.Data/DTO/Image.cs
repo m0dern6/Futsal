12a6205bc8c5b952452cab4368e5c2b1a9d70ea0
0ankit0 <ankitpaudel.1373@gmail.com>
2025-08-12 06:01:49 +0545

    Refactor authentication to use ASP.NET Core Identity
    This commit removes dependencies on the `FutsalApi.Auth.Models` namespace and replaces them with direct usage of ASP.NET Core Identity classes, including `IdentityRole`, `IdentityUser`, and `IdentityUserRole`.
    
    The `AppDbContext` and migration files have been updated to align with the new identity model. New classes and enums, such as `Image` and `ImageEntityType`, have been added to the `FutsalApi.Data.DTO` namespace for better organization.
    
    Additionally, the project file has been modified to include necessary package references for ASP.NET Core Identity and HTTP features, while removing the reference to the `FutsalApi.Auth` project, enhancing modularity and maintainability.

---
+++ FutsalApi/FutsalApi.Data/DTO/Image.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FutsalApi.Data.DTO;
+
+public enum ImageEntityType
+{
+    Review,
+    Ground,
+    User
+}
+public class Image
+{
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+    public string FilePath { get; set; } = default!;
+    public string FileName { get; set; } = default!;
+    public string FileType { get; set; } = default!;
+    public long Size { get; set; }
+    public string UserId { get; set; } = default!;
+    public bool IsDeleted { get; set; }
+}
---
+++ FutsalApi/FutsalApi.Data/DTO/Role.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace FutsalApi.Data.DTO;
+
+public class Role : IdentityRole
+{
+
+}
