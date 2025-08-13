86fa040dbe0b113b44f65f2b78d75d5d92f00ff7
m0dern6 <na.ab.en.153@gmail.com>
2025-08-13 09:39:23 +0545

    fixed top reviewed endpoint

--- futsal_app/lib/core/const/api_const.dart
+++ futsal_app/lib/core/const/api_const.dart
@@ -6,7 +6,7 @@ class ApiConst {
   static const String logout = 'User/logout';
   static const String futsalGround = '/FutsalGround';
   static const String trendingGround = '/FutsalGround/trending';
-  static const String topReviewGround = '/FutsalGround/topreview';
+  static const String topReviewGround = '/FutsalGround/top-reviewed';
   //
   static const String userInfo = 'User/manage/info';
   //
