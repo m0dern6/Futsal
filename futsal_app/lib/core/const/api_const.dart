f1e361d1c1872a395a6ee8f4f2084797f5c5ea8e
m0dern6 <na.ab.en.153@gmail.com>
2025-08-13 09:11:57 +0545

    added trending and top review endpoint

--- futsal_app/lib/core/const/api_const.dart
+++ futsal_app/lib/core/const/api_const.dart
@@ -5,6 +5,8 @@ class ApiConst {
   static const String register = 'User/register';
   static const String logout = 'User/logout';
   static const String futsalGround = '/FutsalGround';
+  static const String trendingGround = '/FutsalGround/trending';
+  static const String topReviewGround = '/FutsalGround/topreview';
   //
   static const String userInfo = 'User/manage/info';
   //
