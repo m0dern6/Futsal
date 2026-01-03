71cfa3cb9b5cb8eb0b47ba1c1b65cb944e12ec89
0ankit0 <ankitpaudel.1373@gmail.com>
2026-01-03 15:37:42 +0545

    feat: update Review entity to use ImageId instead of ImageUrl and add foreign key relationship

---
+++ FutsalApi/FutsalApi.Data/Migrations/20260103000000_UpdateReviewImage.cs
@@ -0,0 +1,57 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FutsalApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateReviewImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Reviews",
+                newName: "ImageId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ImageId",
+                table: "Reviews",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Images_ImageId",
+                table: "Reviews",
+                column: "ImageId",
+                principalTable: "Images",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Images_ImageId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageId",
+                table: "Reviews",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.RenameColumn(
+                name: "ImageId",
+                table: "Reviews",
+                newName: "ImageUrl");
+        }
+    }
+}
